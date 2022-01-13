ab827fdde01f3a024feea9f1ea35ece134d23405
Alex <rodriguezespinosaa@gmail.com>
2022-01-13 16:12:51 +0100

    Find funcional

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const {application} = require('express');
 const express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/';
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db('TavernSkeep');
+
+    // --- BUSCAR ---
+
+    dbo.collection('empleado').findOne({codigo:'123456789ABC'}, function(err, result) {
+        if (err) throw err;
+        console.log(result.nombre);
+        db.close();
+    });
+});
+
 const router = express.Router();
 
 //Data from hypothetical database
-const users = require("../sample.json")
+
+//const users = require("../sample.json");
 
 const server = express();
 
+/* 
 server.use(express.json());
 server.use(express.urlencoded({extended: false}));
 
 server.use(require('./routes/index'));
-server.use("/api/users", require('./routes/users'));
+server.use("/api/users", require('./routes/usuarios'));
 
 router.put('/:id', (peticion, respuesta) =>{
     const {usurname, lastname} = peticion.body;
@@ -26,17 +43,17 @@ router.put('/:id', (peticion, respuesta) =>{
     }else{
         respuesta.json("Error al introducir los datos.");
     }
-})
+});
 
 router.delete('/:id', (peticion, respuesta) =>{
     const filtered = users.filter(function(element){
         if(element.id == peticion.params['id'])
             users.splice(element, 1)
-    })
+    });
     respuesta.send(users)
-})
+});
 
-
+*/
 server.listen(8080, () => {
     console.log('Server on port 8080');
-});
+}); 
